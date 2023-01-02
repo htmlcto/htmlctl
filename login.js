e0d933a0a403984f940abb91def2b8d2895b1c54
KeyPro77 <93216062+KeyPro77@users.noreply.github.com>
2023-01-02 18:48:11 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,22 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "m.walter@aon.at" && password === "Martin1970") {
+        location.href = "https://keypro77.github.io/htmlctm/";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
+
+
+
+
+ 
